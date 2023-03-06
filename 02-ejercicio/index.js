745c9a3c26173b1930e714030643373f5f1c1b53
pitufogranjero <58295885+pitufogranjero@users.noreply.github.com>
2023-03-06 16:04:01 +0100

    02

---
+++ 02-ejercicio/index.js
@@ -0,0 +1,15 @@
+const tuNombre = "Javi Amat";
+console.log(tuNombre);
+
+const tuEdad = 45;
+console.log(tuEdad);
+
+const eresDesarrollador = true;
+console.log(eresDesarrollador);
+
+const nacimiento = "09-31-1978";
+console.log(nacimiento);
+
+const libro = "Mortadelo y Filemón";
+console.log(libro);
+

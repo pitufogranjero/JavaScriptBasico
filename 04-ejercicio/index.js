1b188a400cf04a594a3f533f31c3ab429886fb52
pitufogranjero <58295885+pitufogranjero@users.noreply.github.com>
2023-03-07 15:46:32 +0100

    4c

--- 04-ejercicio/index.js
+++ 04-ejercicio/index.js
@@ -1,19 +1,29 @@
 let nombre = "Javier"
+console.log(nombre)
 
 let apellido = "Amat"
+console.log(apellido)
 
 let estudiante = nombre.concat(" ",apellido)
+console.log(estudiante)
 
 let estudianteMayus = estudiante.toUpperCase()
+console.log(estudianteMayus)
 
 let estudianteMinus = estudiante.toLowerCase()
+console.log(estudianteMinus)
 
-let letras = estudiante.length()
+let letras = estudiante.length
+console.log(letras)
 
 let primera = nombre.charAt(0)
+console.log(primera)
 
-let ultima = apellido.charAt(apellido.length())
+let ultima = apellido.charAt(apellido.length-1)
+console.log(ultima)
 
 let estudiante2 = estudiante.replace(" ","")
+console.log(estudiante2)
 
 let booleana = estudiante.includes(nombre)
+console.log(booleana)
